8379c6bac960aff337d2fe6700f2dccde3c092f0
Naoki-zamir <paredescamilo132@gmail.com>
2025-04-10 20:28:18 -0500

    migracion

---
+++ database/migrations/2025_04_10_235522_create_inputs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inputs', function (Blueprint $table) {
+            $table->id();
+            $table->string('InputName',80);
+            $table->string('UnitMeasurement',10);
+            $table->integer('InitialQuantity');
+            $table->integer('CurrenStock');
+            $table->string('UnitMeasurementGrams',2);
+            $table->double('UnitePrice',10,3);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inputs');
+    }
+};
