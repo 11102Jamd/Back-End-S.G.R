aa60812f691a9c6195d56076326d9931f7dac16d
11102Jamd <jalejandromd17@gmail.com>
2025-06-24 17:35:13 -0500

    Rutas para cada rol establecidas correctamente

--- routes/api.php
+++ routes/api.php
@@ -18,19 +18,30 @@ Route::middleware('auth:sanctum')->group(function () {
         return $request->user();
     });
 
-    Route::middleware(['is_baker'])->group(function () {
-        Route::apiResource('inputs', InputController::class);
-    });
+    /**
+     * Rutas especificas para el rol Administrador
+     */
     Route::middleware(['is_admin'])->group(function () {
+        Route::apiResource('suppliers', SupplierController::class);
         Route::apiResource('inputs', InputController::class);
+        Route::apiResource('purchase', PurchaseOrderController::class)->except(['destroy']);
+        Route::apiResource('products', ProductController::class);
+        Route::apiResource('manufacturing', ManufacturingController::class)->except(['destroy']);
     });
 
+    /**
+     * Rutas especificas para el panadero
+     */
+    Route::middleware(['is_baker'])->group(function () {
+        Route::apiResource('manufacturing', ManufacturingController::class);
+        Route::apiResource('inputs', InputController::class)->only(['index', 'show', 'store', 'update']);
+    });
+
+    /**
+     * Rutas especificas para el Cajero
+     */
+    Route::middleware(['is_cashier'])->group(function () {
+        Route::apiResource('products', ProductController::class)->only(['index', 'show']);
+        Route::apiResource('purchase', PurchaseOrderController::class)->only(['index', 'store']);
+    });
 });
-
-
-Route::apiResource('manufacturing', ManufacturingController::class);
-Route::apiResource('inputs', InputController::class);
-Route::apiResource('purchaseorder', PurchaseOrderController::class);
-Route::apiResource('supplier', SupplierController::class);
-Route::apiResource('product', ProductController::class);
-
