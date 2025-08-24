282c5456c448ae4f2d9ed0f750b75c21b6e7e6b6
plazaone <juandavidplaza514@gmail.com>
2025-08-24 11:31:35 -0500

    Creacion de los modelos con sus relaciones y metodos

---
+++ app/Models/Order.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Bus\Batch;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Order extends Model
+{
+    protected $table = 'order';
+    protected $fillable =
+    [
+        'supplier_name',
+        'order_date',
+        'order_total'
+    ];
+
+    //Relacion de las tablas pivote
+    public function batches(): HasMany
+    {
+        return $this->hasMany(InputBatch::class, 'order_id', 'id');
+    }
+
+}
